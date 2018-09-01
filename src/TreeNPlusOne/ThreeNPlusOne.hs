6bc5ed631c125578b7298f024045421bdbeca907
Carlos Gomez <carliros.g@gmail.com>
2018-08-31 20:25:30 -0400

    First try for memozing (not working)

--- src/TreeNPlusOne/ThreeNPlusOne.hs
+++ src/TreeNPlusOne/ThreeNPlusOne.hs
@@ -5,6 +5,8 @@
 -}
 module Main where
 
+import Data.Function(fix)
+
 main :: IO()
 main = interact $ unlines . calcCycles . map read . words
 
@@ -16,14 +18,35 @@ calcCycles (n1:n2:rest)
     in line : calcCycles rest
 
 getMaxCycle :: Int -> Int -> Int
-getMaxCycle n1 n2 = maximum (map theeNPlusOneLength list)
+getMaxCycle n1 n2 = maximum (map theeNPlusOneLengthMemo list)
   where list = if n1 <= n2 then [n1..n2] else [n1, n1-1..n2]
 
 
-theeNPlusOneLength :: Int -> Int
-theeNPlusOneLength 1 = 1
-theeNPlusOneLength n
-  = 1 + (theeNPlusOneLength m)
-  where m = if even n
-            then n `div` 2
-            else (3 * n) + 1
+memoize :: (Int -> a) -> (Int -> a)
+memoize f = (map f [1..] !!)
+
+memo f = (map f [0..] !!)
+
+theeNPlusOneLength :: (Int -> Int) -> Int -> Int
+theeNPlusOneLength f 1 = 1
+theeNPlusOneLength f 2 = 2
+theeNPlusOneLength f n
+  | even n = 1 + (f (n `div` 2))
+  | odd n  = 1 + (f ((3 * n) + 1))
+
+theeNPlusOneLengthMemo :: Int -> Int
+theeNPlusOneLengthMemo = fix (memoize . theeNPlusOneLength)
+
+factFix f n = if n == 0
+              then 1
+              else (*) n (f (n-1))
+
+fact = fix factFix
+
+factMemo = fix (memo . factFix)
+
+fib f 0 = 0
+fib f 1 = 1
+fib f n = f(n-1) + f(n-2)
+
+fixMemo = fix (memo . fib)
